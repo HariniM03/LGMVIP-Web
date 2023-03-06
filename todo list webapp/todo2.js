5b55b53443cadb3d29c5c95085684960d8bda3b6
HariniM03 <126591751+HariniM03@users.noreply.github.com>
2023-03-06 20:07:03 +0530

    Update todo2.js

--- todo list webapp/todo2.js
+++ todo list webapp/todo2.js
@@ -7,8 +7,9 @@ addTaskBtn.addEventListener('click', function() {
 		let taskItem = document.createElement('li');
 		taskItem.className = 'taskitem';
 		taskItem.innerHTML = `
+			<input type="checkbox">
 			<span>${inputTask.value.trim()}</span>
-			<button>DELETE</button>
+			<button>Delete</button>
 		`;
 		taskList.appendChild(taskItem);
 		inputTask.value = '';
@@ -18,5 +19,14 @@ addTaskBtn.addEventListener('click', function() {
 taskList.addEventListener('click', function(event) {
 	if (event.target.tagName === 'BUTTON') {
 		event.target.parentElement.remove();
+	} else if (event.target.tagName === 'INPUT') {
+		let taskItem = event.target.parentElement;
+		if (event.target.checked) {
+			taskItem.style.textDecoration = 'linethrough';
+			taskItem.style.color = '#aaa';
+		} else {
+			taskItem.style.textDecoration = 'none';
+			taskItem.style.color = '#333';
+		}
 	}
 });
